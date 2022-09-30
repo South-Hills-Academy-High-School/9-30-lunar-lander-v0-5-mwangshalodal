b8fea12011494608ab9e7f8d72c1085f04b0834b
mwangshalodal <113388229+mwangshalodal@users.noreply.github.com>
2022-09-30 14:23:47 -0700

    it dosn't work LOL

--- main.ts
+++ main.ts
@@ -6,23 +6,84 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
     fireball.setFlag(SpriteFlag.Invisible, false)
+    rocketfleg = 1
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
+    if (Math.abs(apple.vx) > 20 || apple.vy > 50) {
+        爆 = sprites.create(img`
+            . . . . 2 2 2 2 2 2 2 2 . . . . 
+            . . . 2 4 4 4 5 5 4 4 4 2 2 2 . 
+            . 2 2 5 5 d 4 5 5 5 4 4 4 4 2 . 
+            . 2 4 5 5 5 5 d 5 5 5 4 5 4 2 2 
+            . 2 4 d d 5 5 5 5 5 5 d 4 4 4 2 
+            2 4 5 5 d 5 5 5 d d d 5 5 5 4 4 
+            2 4 5 5 4 4 4 d 5 5 d 5 5 5 4 4 
+            4 4 4 4 . . 2 4 5 5 . . 4 4 4 4 
+            . . b b b b 2 4 4 2 b b b b . . 
+            . b d d d d 2 4 4 2 d d d d b . 
+            b d d b b b 2 4 4 2 b b b d d b 
+            b d d b b b b b b b b b b d d b 
+            b b d 1 1 3 1 1 d 1 d 1 1 d b b 
+            . . b b d d 1 1 3 d d 1 b b . . 
+            . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
+            . . . 2 2 4 4 4 4 4 2 2 2 . . . 
+            `, SpriteKind.Projectile)
+        爆.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
+        scene.cameraFollowSprite(爆)
+        music.baDing.playUntilDone()
+        pause(2000)
+        game.over(false)
+    } else {
+        if (landingfleg == 0) {
+            油 += 100
+            landingfleg = 1
+        }
+    }
     apple.setVelocity(0, -1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += -15 * (3.14 / 180)
 })
+function explotion () {
+    apple.destroy()
+    mySprite2 = sprites.create(img`
+        . . . . 2 2 2 2 2 2 2 2 . . . . 
+        . . . 2 4 4 4 5 5 4 4 4 2 2 2 . 
+        . 2 2 5 5 d 4 5 5 5 4 4 4 4 2 . 
+        . 2 4 5 5 5 5 d 5 5 5 4 5 4 2 2 
+        . 2 4 d d 5 5 5 5 5 5 d 4 4 4 2 
+        2 4 5 5 d 5 5 5 d d d 5 5 5 4 4 
+        2 4 5 5 4 4 4 d 5 5 d 5 5 5 4 4 
+        4 4 4 4 . . 2 4 5 5 . . 4 4 4 4 
+        . . b b b b 2 4 4 2 b b b b . . 
+        . b d d d d 2 4 4 2 d d d d b . 
+        b d d b b b 2 4 4 2 b b b d d b 
+        b d d b b b b b b b b b b d d b 
+        b b d 1 1 3 1 1 d 1 d 1 1 d b b 
+        . . b b d d 1 1 3 d d 1 b b . . 
+        . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
+        . . . 2 2 4 4 4 4 4 2 2 2 . . . 
+        `, SpriteKind.Text)
+}
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     apple.ay = 20
     fireball.setFlag(SpriteFlag.Invisible, true)
+    rocketfleg = 0
 })
+let mySprite2: Sprite = null
+let 爆: Sprite = null
 let apple: Sprite = null
 let angle = 0
 let fireball: Sprite = null
+let landingfleg = 0
+let rocketfleg = 0
+let 油 = 1000
+let Fuelsprites = textsprite.create(convertToText(油))
+rocketfleg = 0
+landingfleg = 0
 fireball = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -73,6 +134,9 @@ apple.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
 game.onUpdate(function () {
+    Fuelsprites.destroy()
+    Fuelsprites = textsprite.create(convertToText(油))
+    Fuelsprites.setPosition(apple.x, apple.y)
     minimap2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
     minimap.includeSprite(myMinimap, apple, MinimapSpriteScale.MinimapScale)
@@ -80,4 +144,10 @@ game.onUpdate(function () {
     minimap2.setPosition(apple.x - 50, apple.y - 30)
     engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
     fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    if (rocketfleg == 1) {
+        油 += -1
+    }
+    if (apple.y < 400) {
+        landingfleg = 0
+    }
 })
